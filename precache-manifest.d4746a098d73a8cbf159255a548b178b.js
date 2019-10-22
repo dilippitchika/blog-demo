c0be60b78e4c6078a9522341aac98eb4a5b03d57
Dilip Pitchika <dilippitchika@Dilips-MacBook-Air.local>
2019-10-22 21:59:27 +0530

    Updates

---
+++ precache-manifest.d4746a098d73a8cbf159255a548b178b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10b0162079f6b2297f1261b9029d96e3",
+    "url": "/blog-demo/index.html"
+  },
+  {
+    "revision": "7a446bb24acc82f1df8c",
+    "url": "/blog-demo/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "91c46ea88764d39d0438",
+    "url": "/blog-demo/static/js/2.0eec09ff.chunk.js"
+  },
+  {
+    "revision": "7a446bb24acc82f1df8c",
+    "url": "/blog-demo/static/js/main.f3fcf834.chunk.js"
+  },
+  {
+    "revision": "1c65e888de07480c52a3",
+    "url": "/blog-demo/static/js/runtime-main.ecd03fa9.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/blog-demo/static/media/logo.25bf045c.svg"
+  }
+]);
