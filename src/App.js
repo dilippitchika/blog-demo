5eba6205e886bbd3f80f0bd5137df822d7b3d828
Dilip Pitchika <dilippitchika@Dilips-MacBook-Air.local>
2019-10-26 11:47:35 +0530

    Made responsive

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <p className="info">Hey, great to see you here.  I have been working since the past 8 months as the associate product manager for IMI’s bot building platform for enterprises, which serves the likes of a major furniture retail store in the UK and a large government organization in Dubai.</p>
         <p className="info">I was previously a designer within IMI entrusted with the creating a seamless experience for building bots within IMI. I helped my team build a consistent interface adapting an existing design system to fit our needs.</p>
         <p className="info">I graduated from Indian Institute of Technology, Kharagpur as a with a Bachelor’s and Master’s degree in Mechanical Engineering in 2018. I am in the active pursuit of heist novels, movies and television shows and I enjoy cooking and jamming on my ukuelele.</p>
-        <div>
+        <div className="buttonsHolder">
           <a className="homeButton" href="https://www.linkedin.com/in/dilip-pitchika" target="_blank">LinkedIn</a>
           <a className="homeButton">Resume</a>
         </div>
